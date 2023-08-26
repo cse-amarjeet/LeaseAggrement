3026000d66bc0cc74eee494607a6f19b1f6fba07
cse-amarjeet <ajroni98@gmail.com>
2023-08-26 11:34:32 +0530

    last touch

--- contracts/LeaseAgreement.sol
+++ contracts/LeaseAgreement.sol
@@ -1,75 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract LeaseAgreement {
-    address public landlord;
-    address public tenant;
-    
-    uint256 public monthlyRent;
-    uint256 public leaseDuration; // in months
-    uint256 public startDate;
-    uint256 public endDate;
-    bool public leaseTerminated;
-    bool public leaseActive;
-    string public aggrementName;
-    string public ownerName;
-    string public tenantName;
-    
-    modifier onlyLandlord() {
-        require(msg.sender == landlord, "Only the landlord can call this function.");
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract Proposal {
+    string public subject;
+    address public tokenAddress;
+    address[] public voters;
+    mapping(address => bool) public voted;
+    uint256 public requiredVotes;
+    uint256 public votePrice = 5;
+    uint256 public upVoteCount;
+    uint256 public downVoteCount;
+    bool public proposalStatus=false;
+
+    constructor(string memory _subject, address _tokenAddress, uint _requiredVotes) {
+        subject = _subject;
+        requiredVotes = _requiredVotes; // Adjust as needed
+        tokenAddress = _tokenAddress;
+    }
+
+    modifier onlyVoters() {
+        require(isValidVoter(msg.sender), "Invalid voter");
         _;
     }
-    
-    modifier onlyTenant() {
-        require(msg.sender == tenant, "Only the tenant can call this function.");
-        _;
+
+    function isValidVoter(address student) internal view returns (bool) {
+        VotingDAOFactory factory = VotingDAOFactory(tokenAddress);
+        if(factory.lastMintTimestamp(student) == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
-    
-    modifier leaseIsActive() {
-        require(leaseActive, "Lease is not active.");
-        _;
-    }
-    
-    constructor(
-        string memory _aggrementName,
-        string memory _ownerName,
-        string memory _tenantName,
-        address _tenant,
-        uint256 _monthlyRent,
-        uint256 _leaseDurationMonths
-    ) {
-        landlord = tx.origin;
-        tenant = _tenant;
-        monthlyRent = _monthlyRent;
-        leaseDuration = _leaseDurationMonths * 30 days;
-        startDate = block.timestamp;
-        endDate = startDate + leaseDuration;
-        leaseTerminated = false;
-        leaseActive = true;
-        aggrementName=_aggrementName;
-        tenantName=_tenantName;
-        ownerName=_ownerName;
-    }
-    
-    function makePayment() external payable onlyTenant leaseIsActive {
-        require(msg.value == monthlyRent, "Incorrect payment amount.");
-        // Log payment event or update payment history
+
+    function vote(bool inFavor) public  {
+        VotingDAOFactory factory = VotingDAOFactory(tokenAddress);
+        require(!voted[msg.sender], "Already voted");
+        require(factory.balanceOf(msg.sender) >= votePrice, "Not enough balance");
         
-        if (block.timestamp >= endDate) {
-            leaseTerminated = true;
-            leaseActive = false;
+        // Debit the votePrice tokens from the sender
+        factory.internalBurn(msg.sender, votePrice);
+
+        voted[msg.sender] = true;
+        voters.push(msg.sender);
+
+        if (inFavor) {
+            upVoteCount=upVoteCount+1;
+        } else {
+            downVoteCount=downVoteCount+1;
         }
-        payable(landlord).transfer(msg.value);
-    }
-    function terminateLease() external onlyLandlord {
-        require(!leaseTerminated, "Lease is already terminated.");
-        leaseTerminated = true;
-        leaseActive = false;
-    }
-    function getRemainingLeaseDuration() external view returns (uint256) {
-        if (block.timestamp >= endDate) {
-            return 0;
+        if(upVoteCount>=requiredVotes){
+            proposalStatus=true;
         }
-        return endDate - block.timestamp;
-    }
+ }
 }
